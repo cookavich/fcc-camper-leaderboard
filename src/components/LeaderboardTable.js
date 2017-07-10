1eccb2c982bbbf58d18988a3618b38ef727d7e52
cookavich <paulrcook@me.com>
2017-07-10 17:41:58 -0400

    fixed jsx error and removed .idea from repo

--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import LeaderRow from 'LeaderRow.js';
+import LeaderRow from './LeaderRow.js';
+import Axios from 'axios';
 
 class LeaderboardTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             recentLeaders: [],
-            test: 1
         };
     }
 
     componentDidMount() {
-        axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+        Axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
             .then(res => {
                 this.setState({
-                    recentLeaders: res
+                    recentLeaders: res.data
                 })
             })
     }
 
     render() {
         return (
-            <LeaderRow/>
+            <div>
+                {this.state.recentLeaders && <LeaderRow/>}
+            </div>
         )
     }
 }
